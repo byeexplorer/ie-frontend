fa6f4cfd4c7eba91a913ac13209cf3ec71b18081
hwangyena <hwangyena@sk.com>
2022-07-31 22:35:13 +0900

    feat: make 2006 transition using gsap

--- components/timeline/time-2006.tsx
+++ components/timeline/time-2006.tsx
@@ -1,14 +1,96 @@
-type Props = {};
+import { memo, useEffect } from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/dist/ScrollTrigger';
 
-const Time2006 = (props: Props) => {
+const Time2006 = () => {
+  useEffect(() => {
+    gsap.registerPlugin(ScrollTrigger);
+
+    gsap.from('#first', {
+      x: 0,
+      y: '20%',
+      rotate: 10,
+    });
+    gsap.to('#first', {
+      x: 0,
+      y: '-20%',
+      rotate: -10,
+      scrollTrigger: {
+        trigger: '#first',
+        start: 'top bottom',
+        scrub: true,
+      },
+    });
+
+    gsap.from('#second', {
+      x: 0,
+      y: '40%',
+      rotate: 20,
+    });
+    gsap.to('#second', {
+      x: 0,
+      y: '-40%',
+      rotate: -20,
+      scrollTrigger: {
+        trigger: '#second',
+        start: 'top bottom',
+        scrub: true,
+      },
+    });
+
+    gsap.from('#third', {
+      x: 0,
+      y: '60%',
+      rotate: 30,
+    });
+    gsap.to('#third', {
+      x: 0,
+      y: '-60%',
+      rotate: -30,
+      scrollTrigger: {
+        trigger: '#third',
+        start: 'top bottom',
+        scrub: true,
+      },
+    });
+    gsap.from('#fourth', {
+      x: 0,
+      y: '70%',
+      rotate: 50,
+    });
+    gsap.to('#fourth', {
+      x: 0,
+      y: '-80%',
+      rotate: -50,
+      scrollTrigger: {
+        trigger: '#fourth',
+        start: 'top bottom',
+        scrub: true,
+      },
+    });
+  }, []);
+
   return (
-    <section className="bg-blue flex justify-center gap-5">
-      <span className="text-[11rem]">2</span>
-      <span className="text-[11rem]">0</span>
-      <span className="text-[11rem]">0</span>
-      <span className="text-[11rem]">6</span>
-    </section>
+    <>
+      <section className="bg-blue flex justify-center gap-6 mt-5 ">
+        <span className="text-[11rem] leading-[7.5rem]" id="first">
+          2
+        </span>
+        <span className="text-[11rem] leading-[7.5rem]" id="second">
+          0
+        </span>
+        <span className="text-[11rem] leading-[7.5rem]" id="third">
+          0
+        </span>
+        <span className="text-[11rem] leading-[7.5rem]" id="fourth">
+          6
+        </span>
+      </section>
+      <section>
+        <div className="w-[30vw] h-[30vw] bg-slate-400 mt-[50vh] mb-[13vw]" />
+      </section>
+    </>
   );
 };
 
-export default Time2006;
+export default memo(Time2006);
--- pages/index.tsx
+++ pages/index.tsx
@@ -4,7 +4,7 @@ export default function MainPage() {
   return (
     <div>
       <article className="bg-blue grid place-items-center pt-[20%]">
-        <section className="text-xs font-thin leading-[140%]  mb-[20%] text-center">
+        <section className="text-xs font-thin leading-[140%]  mb-[30%] text-center">
           In July 1995 Microsoft released Internet Explorer 1.0
           <br /> as an add-on to the Windows 95 operating system.
           <br /> In July 1995 Microsoft released Internet Explorer 1.0
