5d6cbad72dcfeea87d08e05cf1eb1a36cc83aa74
hwangyena <hwangyena@sk.com>
2022-07-31 23:18:36 +0900

    feat: add floating icons

--- components/timeline/index.ts
+++ components/timeline/index.ts
@@ -1,1 +1,2 @@
 export { default as Time2006 } from './time-2006';
+export { default as FloatingIcon } from './floating-icon';
--- next.config.js
+++ next.config.js
@@ -1,10 +1,14 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
+const path = require('path');
 
 const nextConfig = {
   reactStrictMode: true,
   sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
+    includePaths: [path.join(__dirname, 'styles')],
+  },
+  images: {
+    loader: 'akamai',
+    path: '',
   },
 };
 
