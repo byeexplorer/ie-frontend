f320d5206ed23953f773d3da103d66386ea41817
hwangyena <hwangyena@sk.com>
2022-08-28 14:43:22 +0900

    feat: add guest card

---
+++ components/common/guest-card.tsx
@@ -0,0 +1,52 @@
+import { memo, useState } from 'react';
+import styles from 'styles/guestcard.module.scss';
+
+type Props = {
+  type: 'blue' | 'green' | 'purple' | 'indigo';
+  isView?: boolean;
+  userName?: string;
+  userComment?: string;
+};
+
+const Mode1 = ({ comment, name }: { name: string; comment: string }) => {
+  return (
+    <div className={styles.view}>
+      <h3>{name}</h3>
+      <p>{comment}</p>
+    </div>
+  );
+};
+const ViewMode = memo(Mode1);
+
+const WritingMode = () => {
+  // TODO: check design
+  const [name, setName] = useState('');
+  const [message, setMessage] = useState('');
+
+  return (
+    <div className={styles.writing}>
+      <label>
+        Name
+        <input value={name} onChange={(e) => setName(e.target.value)} />
+      </label>
+      <label>
+        Comment
+        <input value={message} onChange={(e) => setMessage(e.target.value)} />
+      </label>
+    </div>
+  );
+};
+
+const GuestCard = ({ userComment, userName, type, isView = false }: Props) => {
+  return (
+    <div className={styles.guestcard}>
+      <section className={`${styles.gradient} ${styles[type]}`} />
+      <section className={styles.cardcontent}>
+        <div className={`${styles.profile} ${styles['profile-' + type]}`}></div>
+        {isView ? <ViewMode name={userName ?? ''} comment={userComment ?? ''} /> : <WritingMode />}
+      </section>
+    </div>
+  );
+};
+
+export default memo(GuestCard);
--- types/index.d.ts
+++ types/index.d.ts
@@ -4,3 +4,24 @@ type Team = {
   job: string;
   comment: string;
 };
+
+type CardBlue = {
+  type: 'blue';
+  color: '#6702ff';
+};
+type CardGreen = {
+  type: 'green';
+  color: '#485A00';
+};
+type CardPurple = {
+  type: 'purple';
+  color: '#8c20b1';
+};
+type CardIndigo = {
+  type: 'indigo';
+  color: '#6792FF';
+};
+
+type GuestCard = {
+  type: 'blue' | 'green' | 'purple' | 'indigo';
+};
