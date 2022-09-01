403c45171ae1dba2cc3787f70cbbea34280e4dda
Hwang yena <hwangyena@sk.com>
2022-09-01 13:49:11 +0900

    fix: remove test code in otherpeople

--- components/otherpeople.tsx
+++ components/otherpeople.tsx
@@ -8,9 +8,10 @@ const Otherpeople = (props: Props) => {
   return (
     <section>
       <h1>You can see other people!</h1>
-      <GuestCard type="blue" userName="황예나" userComment="와 페이지 너무 이뻐요~ 와 페이지 너무 이뻐요~" />
+      <p className="text-[15px]">작업중입니다.</p>
+      {/* <GuestCard type="blue" userName="황예나" userComment="와 페이지 너무 이뻐요~ 와 페이지 너무 이뻐요~" />
       <GuestCard type="gray" userName="황예나" userComment="와 페이지 너무 이뻐요~ 와 페이지 너무 이뻐요~" />
-      <GuestCard type="purple" userName="황예나" userComment="와 페이지 너무 이뻐요~ 와 페이지 너무 이뻐요~" />
+      <GuestCard type="purple" userName="황예나" userComment="와 페이지 너무 이뻐요~ 와 페이지 너무 이뻐요~" /> */}
     </section>
   );
 };
