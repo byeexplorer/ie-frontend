d430d453389276273a590ec4dab83a0d81c7cde4
hwangyena <center7332@naver.com>
2022-09-26 14:07:47 +0900

    feat: add favicon

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -3,6 +3,9 @@ import type { AppProps } from 'next/app';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps }: AppProps) {
+  <Head>
+    <link rel="icon" href="/favicon.ico" />
+  </Head>;
   return <Component {...pageProps} />;
 }
 
