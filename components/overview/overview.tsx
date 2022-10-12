61372abc811507a2cf0bfee81defd9e6ade2caa8
김형태(Hyeongtae Kim)/AI솔루션개발팀/SKP <hyeongtae.kim@sk.com>
2022-10-12 22:41:41 +0900

    fix overview font size

--- components/overview/overview.tsx
+++ components/overview/overview.tsx
@@ -32,15 +32,12 @@ const Overview = () => {
       </section> */}
       <section id="intro" className="relative">
         <div className="absolute w-screen h-screen flex justify-center items-center">
-          <p className="text-[1.6rem] leading-[2.2rem] text-center">
-            Microsoft is finally retiring
-            <br />
-            consumer version of Internet Explorer.
-            <br />
-            It announced the plan last year,
-            <br />
-            making Internet Explorer 11 its final version.
-          </p>
+          <div className="text-[5vw] leading-[8vw] text-center">
+            <p>Microsoft is finally retiring</p>
+            <p>consumer version of Internet Explorer.</p>
+            <p>It announced the plan last year,</p>
+            <p>making Internet Explorer 11 its final version.</p>
+          </div>
         </div>
         <div className="absolute w-screen h-screen flex justify-center items-center">
           <p className="text-[0.7rem] leading-[1rem]">Hello, Explorer!</p>
