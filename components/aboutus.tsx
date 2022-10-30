3516c47b1d985e15f9c5bc7932de0235eff01e92
Hwang yena <center7332@naver.com>
2022-10-30 16:24:23 +0900

    feature: add email

--- components/aboutus.tsx
+++ components/aboutus.tsx
@@ -8,6 +8,7 @@ const teams: Team[] = [
     job: 'BX Designer',
     comment: '브랜드 디자이너 김다미입니다.<br />Visual Communication Design을 탐구합니다.',
     profile: './images/profile/dami.jpeg',
+    email: 'kimdm5198@gmail.com',
   },
   {
     id: 2,
@@ -15,6 +16,7 @@ const teams: Team[] = [
     job: 'Frontend Developer',
     comment: '프론트엔드 개발자 김형태입니다.',
     profile: './images/profile/hyeontae.jpeg',
+    email: 'dev.hyongti@gmail.com',
   },
   {
     id: 3,
@@ -22,6 +24,7 @@ const teams: Team[] = [
     job: 'BX Designer',
     comment: '모션디자이너 정성은입니다.',
     profile: './images/profile/seongeun.jpeg',
+    email: 'test@naver.com',
   },
   {
     id: 4,
@@ -29,6 +32,7 @@ const teams: Team[] = [
     job: 'Frontend Developer',
     comment: '프론트엔드 개발자 황예나입니다. <br/>이해를 동반한 코드를 작성합니다.',
     profile: './images/profile/yena.jpeg',
+    email: 'center7332@gmail.com',
   },
 ];
 
@@ -40,6 +44,10 @@ const Aboutus = () => {
     setSelectId(id);
   };
 
+  const handleEmail = (email: string) => {
+    window.open(`mailto:${email}`);
+  };
+
   return (
     <article id="host-explorer" className="ml-[13%] h-screen flex justify-center flex-col">
       <section className="mb-4">
@@ -48,7 +56,6 @@ const Aboutus = () => {
       </section>
       <section className="flex gap-9 w-full">
         <div className="w-[258px]">
-          {/* TODO: insert image */}
           <div className="w-full h-[341px] bg-[#3c3c3c] rounded-md relative">
             <Image
               src={teams.filter((v) => v.id === selectId)[0].profile}
@@ -60,9 +67,16 @@ const Aboutus = () => {
           <h2 className="text-xs mt-1">{target.name}</h2>
           <h4 className="text-[21px]">{target.job}</h4>
           <p className="mt-[14px] text-[14px]" dangerouslySetInnerHTML={{ __html: target.comment }} />
+          <button
+            className="mt-[16px] text-[10px] flex items-center gap-[4px] w-full justify-end"
+            onClick={() => handleEmail(teams.filter((v) => v.id === selectId)[0].email)}
+          >
+            <Image src="/icons/email.svg" alt="email" width={16} height={12} layout="fixed" />
+            <p>E-mail</p>
+          </button>
         </div>
         <div className="w-[60%]">
-          <ul className="w-full flex gap-2 flex-col">
+          <ul className="w-full flex gap-2 flex-col ">
             {teams.map((team) => (
               <li
                 key={team.id}
