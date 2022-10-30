ee58585d84666308fd92064e5f8fda6c08691ef4
Hwang yena <center7332@naver.com>
2022-10-30 16:35:00 +0900

    feat: add SEO

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -4,7 +4,19 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
-          <link rel="icon" href="/favicon.ico" />
+          <title>Bye, explorer</title>
+          <meta name="description" content="인터넷 익스플로러의 종료를 기념하는 웹페이지" />
+          <meta name="viewport" content="width=device-width,initial-scale=1" />
+          <link rel="icon" href="/favicon.ico" sizes="any" />
+          {/* <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" /> */}
+          <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-icon.png" />
+          <link rel="manifest" href="/assets/favicon/manifest.json" />
+          <meta name="og:title" content="Bye, explorer" />
+          <meta name="og:description" content="bye, Internet explorer" />
+          <meta name="og:type" content="website" />
+          <meta name="og:url" content="https://bye-explorer.web.app/" />
+          <meta name="og:image" content="/images/navbar/blue.png" />
+          <meta property="og:article:author" content="bye exlorer" />
         </Head>
         <body>
           <Main />
