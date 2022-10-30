a1ec1e2986533fc3cf64f95aa759ad930acff1d6
Hwang yena <center7332@naver.com>
2022-10-30 23:14:10 +0900

    fix: SEO

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -22,14 +22,12 @@ function App({ Component, pageProps }: AppProps) {
     <>
       <Head>
         <title>Bye, explorer</title>
-        <meta name="description" content="인터넷 익스플로러의 종료를 기념하는 웹페이지" />
+        <meta name="description" content="Bye, Internet explorer" />
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" sizes="any" />
-        {/* <link rel="icon" type="image/png" sizes="32x32" href="/assets/favicon/favicon-32x32.png" /> */}
-        <link rel="apple-touch-icon" sizes="180x180" href="/assets/favicon/apple-icon.png" />
-        <link rel="manifest" href="/assets/favicon/manifest.json" />
+        <link rel="apple-touch-icon" sizes="180x180" href="/favicon.ico" />
         <meta name="og:title" content="Bye, explorer" />
-        <meta name="og:description" content="bye, Internet explorer" />
+        <meta name="og:description" content="Bye, Internet explorer" />
         <meta name="og:type" content="website" />
         <meta name="og:url" content="https://bye-explorer.web.app/" />
         <meta name="og:image" content="/images/navbar/blue.png" />
